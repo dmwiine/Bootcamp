f8f87e4e79f1ed9397f96061849a174757ac835d
Donna Mwiine <dmwiine@gmail.com>
2017-04-26 10:08:29 +0300

    Changed the print allocations function and added a .join() function

--- app/living_space.py
+++ app/living_space.py
@@ -3,17 +3,23 @@ class LivingSpace(Room):
     def __init__(self,name):
         super().__init__(name)
         self.space_available = 4
+        self.occupants = []
 
 
+    def has_space(self):
+        if self.space_available == 0:
+            return False
+        else:
+            return True
 
-        def has_space(self):
-            if self.space_available == 0:
-                return False
-            else:
-                return True
+    def add_person(self,fellow):
+        fellow.living_space = self
+        if self.space_available != 0:
+            self.space_available -= 1
+            self.occupants.append(fellow)
+        return fellow
 
-        def add_person(self,fellow):
-            fellow.living_space = self
-            if self.space_available != 0:
-                self.space_available -= 1
-            return fellow
+    def print_allocations(self):
+        print(self.name)
+        print("-----------------------------------------------------")
+        print(",".join(occupant.name for occupant in self.occupants))
